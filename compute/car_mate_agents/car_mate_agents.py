dd43b3cfc302f120059faa21e81cd918fd5d3a7a
Jan Franck <jan.f.1996@web.de>
2025-09-30 19:32:47 +0200

    ai agent structure

---
+++ compute/car_mate_agents/car_mate_agents.py
@@ -0,0 +1,85 @@
+"""
+SPDX-FileCopyrightText: 2024 Contributors to the Eclipse Foundation
+
+See the NOTICE file(s) distributed with this work for additional
+information regarding copyright ownership.
+
+This program and the accompanying materials are made available under the
+terms of the Apache License Version 2.0 which is available at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+SPDX-License-Identifier: Apache-2.0
+"""
+
+import asyncio
+import time
+
+from uprotocol.communication.inmemoryrpcserver import InMemoryRpcServer
+from uprotocol.communication.requesthandler import RequestHandler
+from uprotocol.communication.upayload import UPayload
+from uprotocol.v1.uattributes_pb2 import (
+    UPayloadFormat,
+)
+from uprotocol.v1.umessage_pb2 import UMessage
+from uprotocol.v1.uri_pb2 import UUri
+
+from up_transport_zenoh.examples import common_uuri
+from up_transport_zenoh.examples.common_uuri import create_method_uri, get_zenoh_default_config
+from up_transport_zenoh.uptransportzenoh import UPTransportZenoh
+
+
+from openai import OpenAI
+
+client = OpenAI(
+  api_key=""
+)
+
+
+
+
+
+source = UUri(authority_name="command", ue_id=18)
+transport = UPTransportZenoh.new(get_zenoh_default_config(), source)
+
+
+
+
+class MyRequestHandler(RequestHandler):
+    async def handle_request(self, msg: UMessage) -> UPayload:
+        common_uuri.logging.debug("Request Received by Service Request Handler")
+        attributes = msg.attributes
+        payload = msg.payload
+        source = attributes.source
+        sink = attributes.sink
+        common_uuri.logging.debug(f"Receive {payload} from {source} to {sink}")
+
+        answer = client.responses.create(
+        model="gpt-4o-mini",
+        input=payload,
+        store=True,
+        )
+
+        # answer = "test answer"
+
+        common_uuri.logging.debug(f"AI Answer {answer.output_text}")
+
+        print(answer.output_text)
+
+
+        payload = UPayload(data=bytes([ord(c) for c in answer.output_text]), format=UPayloadFormat.UPAYLOAD_FORMAT_TEXT)
+        return payload
+
+
+async def register_rpc():
+    uuri = create_method_uri()
+    rpc_server = InMemoryRpcServer(transport)
+    status = await rpc_server.register_request_handler(uuri, MyRequestHandler())
+    common_uuri.logging.debug(f"Request Handler Register status {status}")
+
+    while True:
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    asyncio.run(register_rpc())
