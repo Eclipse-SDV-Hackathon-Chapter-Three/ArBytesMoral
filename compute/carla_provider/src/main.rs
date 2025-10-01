7a1c1557e9e0827c05d54db11a5dbab41829939e
Christopher Schwager <christopher.schwager@carbyte.de>
2025-10-01 10:28:53 +0200

    Add kuksa write from carla for wetness

--- compute/carla_provider/src/main.rs
+++ compute/carla_provider/src/main.rs
@@ -12,63 +12,16 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-use tokio::sync::mpsc;
 use std::time::Duration;
 use kuksa_rust_sdk::kuksa::common::ClientTraitV2;
 use kuksa_rust_sdk::kuksa::val::v2::KuksaClientV2;
 use kuksa_rust_sdk::v2_proto;
-use std::fmt;
 use carla::client::Client;
 use carla::client::ActorBase;
-//use tokio::time::{sleep, Duration};
-use std::sync::atomic::Ordering;
 use log;
 
 const CLIENT_TIME_MS: u64 = 5_000;
 const POLLING_EGO_MS: u64 = 1_000;
-
-struct DisplayDatapoint(v2_proto::Value);
-
-fn display_array<T>(f: &mut fmt::Formatter<'_>, array: &[T]) -> fmt::Result
-where
-    T: fmt::Display,
-{
-    f.write_str("[")?;
-    let real_delimiter = ", ";
-    let mut delimiter = "";
-    for value in array {
-        write!(f, "{delimiter}")?;
-        delimiter = real_delimiter;
-        write!(f, "{value}")?;
-    }
-    f.write_str("]")
-}
-
-impl fmt::Display for DisplayDatapoint {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match &self.0.typed_value {
-            Some(value) => match value {
-                v2_proto::value::TypedValue::Bool(value) => f.pad(&format!("{value}")),
-                v2_proto::value::TypedValue::Int32(value) => f.pad(&format!("{value}")),
-                v2_proto::value::TypedValue::Int64(value) => f.pad(&format!("{value}")),
-                v2_proto::value::TypedValue::Uint32(value) => f.pad(&format!("{value}")),
-                v2_proto::value::TypedValue::Uint64(value) => f.pad(&format!("{value}")),
-                v2_proto::value::TypedValue::Float(value) => f.pad(&format!("{value:.2}")),
-                v2_proto::value::TypedValue::Double(value) => f.pad(&format!("{value}")),
-                v2_proto::value::TypedValue::String(value) => f.pad(&format!("'{value}'")),
-                v2_proto::value::TypedValue::StringArray(array) => display_array(f, &array.values),
-                v2_proto::value::TypedValue::BoolArray(array) => display_array(f, &array.values),
-                v2_proto::value::TypedValue::Int32Array(array) => display_array(f, &array.values),
-                v2_proto::value::TypedValue::Int64Array(array) => display_array(f, &array.values),
-                v2_proto::value::TypedValue::Uint32Array(array) => display_array(f, &array.values),
-                v2_proto::value::TypedValue::Uint64Array(array) => display_array(f, &array.values),
-                v2_proto::value::TypedValue::FloatArray(array) => display_array(f, &array.values),
-                v2_proto::value::TypedValue::DoubleArray(array) => display_array(f, &array.values),
-            },
-            None => f.pad("None"),
-        }
-    }
-}
 
 #[tokio::main]
 async fn main() {
@@ -87,7 +40,7 @@ async fn main() {
     carla_client.set_timeout(Duration::from_millis(CLIENT_TIME_MS));
 
     // Configure Carla's World
-    let mut carla_world = carla_client.world();
+    let carla_world = carla_client.world();
 
     // Wait for the Ego Vehicle actor
     let mut ego_vehicle_id: Option<u32> = None;
@@ -122,10 +75,15 @@ async fn main() {
     let weather = carla_world.weather();
 
     loop {
-        log::info!(
-                        "Wetness: {}",
-                        weather.wetness
-                    );
+        log::info!("Wetness: {}", weather.wetness);
+
+        let _ =v2_client.publish_value(
+            "Vehicle.Exterior.HVAC.Humidity".to_owned(),
+            v2_proto::Value {
+                typed_value: Some(v2_proto::value::TypedValue::Float(weather.wetness)),
+            },
+        ).await;
+
         tokio::time::sleep(Duration::from_millis(500)).await;
     }
 }
